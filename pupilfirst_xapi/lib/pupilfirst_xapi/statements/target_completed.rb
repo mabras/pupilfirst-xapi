e6afb94c74f4731aed977cc4396995387f5e70d3
Mirosław Pragłowski <m@praglowski.com>
2021-02-26 11:32:49 +0100

    Minor refactor, optimize reads from repository

--- pupilfirst_xapi/lib/pupilfirst_xapi/statements/target_completed.rb
+++ pupilfirst_xapi/lib/pupilfirst_xapi/statements/target_completed.rb
@@ -7,14 +7,16 @@ module PupilfirstXapi
       end
 
       def call(actor_id:, resource_id:)
-        actor = @repository.call(:user, actor_id)
         submission = @repository.call(:timeline_event, resource_id)
         return unless submission.passed?
+
+        actor = @repository.call(:user, actor_id)
+        target = submission.target
 
         Xapi.create_statement(
           actor: Actors.agent(actor),
           verb: Verbs::COMPLETED_ASSIGNMENT,
-          object: Objects.target(submission.target, @uri_for.call(submission.target))
+          object: Objects.target(target, @uri_for.call(target))
         )
       end
     end
