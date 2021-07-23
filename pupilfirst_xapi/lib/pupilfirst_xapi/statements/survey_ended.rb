26d7f6dca92ec17cbec421848fdd0150bac5f12d
Charles Washington <charles_was_7@hotmail.com>
2021-07-23 09:08:08 -0300

    Add SurveyEnded statement.

---
+++ pupilfirst_xapi/lib/pupilfirst_xapi/statements/survey_ended.rb
@@ -0,0 +1,21 @@
+module PupilfirstXapi
+  module Statements
+    class SurveyEnded
+      def initialize(repository, uri_for)
+        @repository = repository
+        @uri_for = uri_for
+      end
+
+      def call(actor_id:, resource_id:)
+        actor = @repository.call(:user, actor_id)
+        survey = @repository.call(:survey, resource_id)
+
+        Xapi.create_statement(
+          actor: Actors.agent(actor),
+          verb: Verbs::COMPLETED,
+          object: Objects.survey_start(survey, @uri_for)
+        )
+      end
+    end
+  end
+end
