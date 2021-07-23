989b17fdf23b2574baa3aa3880f3eb2aeec63642
Charles Washington <charles_was_7@hotmail.com>
2021-07-23 08:36:06 -0300

    Add SurveyStart object. This object describes the SurveyStart event. It also adds the survey_slug as an extension.

---
+++ pupilfirst_xapi/lib/pupilfirst_xapi/objects/survey_start.rb
@@ -0,0 +1,18 @@
+module PupilfirstXapi
+  module Objects
+    class SurveyStart
+      def call(survey, uri_for)
+        target_uri = uri_for.call(survey)
+
+        Builder.new(
+          id: target_uri,
+          type: 'http://adlnet.gov/expapi/activities/assessment',
+          name: "survey #{survey.name}",
+          description: survey.description
+        ).with_extension(
+          'survey_slug', survey.slug
+        ).call
+      end
+    end
+  end
+end
