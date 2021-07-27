b26edec68bbee58c9701fe89ff49f8164909df57
Charles Washington <charles_was_7@hotmail.com>
2021-07-27 06:41:38 -0300

    Adjust code to changes. Answer is better than QuestionAnswer

--- pupilfirst_xapi/lib/pupilfirst_xapi/objects/answer.rb
+++ pupilfirst_xapi/lib/pupilfirst_xapi/objects/answer.rb
@@ -1,16 +1,16 @@
 module PupilfirstXapi
   module Objects
-    class QuestionAnswer
-      def call(question_answer, uri_for)
-        target_uri = uri_for.call(question_answer)
+    class Answer
+      def call(answer, uri_for)
+        target_uri = uri_for.call(answer)
 
         Builder.new(
           id: target_uri,
           type: 'http://adlnet.gov/expapi/activities/assessment',
-          name: "question answered #{question_answer.question_description}",
-          description: question_answer.question_description
-        ).with_extension('answer_id', question_answer.answer_id)
-          .with_extension('http://adlnet.gov/expapi/activities/question', question_answer.question_description)
+          name: answer.question_description,
+          description: answer.question_description
+        ).with_extension('answer_id', answer.answer_id)
+          .with_extension('http://adlnet.gov/expapi/activities/question', answer.question_description)
           .call
       end
     end
