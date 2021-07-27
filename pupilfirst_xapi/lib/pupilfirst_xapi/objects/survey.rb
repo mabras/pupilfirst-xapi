c6a3876079a4ca0fe20be5df4fed18ea1b4627ba
Charles Washington <charles_was_7@hotmail.com>
2021-07-27 05:46:39 -0300

    Rename class. Also use survey name.

--- pupilfirst_xapi/lib/pupilfirst_xapi/objects/survey.rb
+++ pupilfirst_xapi/lib/pupilfirst_xapi/objects/survey.rb
@@ -1,13 +1,13 @@
 module PupilfirstXapi
   module Objects
-    class CapabilityResultView
+    class Survey
       def call(survey, uri_for)
         target_uri = uri_for.call(survey)
 
         Builder.new(
           id: target_uri,
-          type: 'http://adlnet.gov/expapi/activities/resource',
-          name: "survey #{survey.name}",
+          type: 'http://id.tincanapi.com/activitytype/survey',
+          name: "#{survey.name}",
           description: survey.description
         ).call
       end
