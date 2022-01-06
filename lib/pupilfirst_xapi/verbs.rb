32af3ee31904fe0b839de3295906be8b63f9e0a3
Mohamad Abras <mabras@users.noreply.github.com>
2022-01-06 14:36:57 +0200

    change video ended verb to watched

--- lib/pupilfirst_xapi/verbs.rb
+++ lib/pupilfirst_xapi/verbs.rb
@@ -4,7 +4,7 @@ module PupilfirstXapi
     COMPLETED_ASSIGNMENT  = Xapi.create_verb(id: 'https://w3id.org/xapi/dod-isd/verbs/completed-assignment', name: 'completed assignment')
     REGISTERED            = Xapi.create_verb(id: 'http://adlnet.gov/expapi/verbs/registered', name: 'registered')
     VIDEO_STARTED         = Xapi.create_verb(id: 'http://activitystrea.ms/schema/1.0/start', name: 'start')
-    VIDEO_ENDED           = Xapi.create_verb(id: 'http://activitystrea.ms/schema/1.0/consume', name: 'consumed')
+    VIDEO_ENDED           = Xapi.create_verb(id: 'http://adlnet.gov/expapi/verbs/watched', name: 'watched')
     STARTED               = Xapi.create_verb(id: 'http://adlnet.gov/expapi/verbs/initialized', name: 'initialized')
     VIEWED                = Xapi.create_verb(id: 'http://id.tincanapi.com/verb/viewed', name: 'viewed')
     ANSWERED              = Xapi.create_verb(id: 'https://w3id.org/xapi/dod-isd/verbs/answered', name: 'answered')
