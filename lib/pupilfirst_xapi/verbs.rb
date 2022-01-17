21d75096a536de5ffe7214f26e4902a4c0900278
Mohamad Abras <mohamad.abras@gmail.com>
2022-01-17 18:41:53 +0200

    change 'completed assignment' to 'attempted'

--- lib/pupilfirst_xapi/verbs.rb
+++ lib/pupilfirst_xapi/verbs.rb
@@ -1,7 +1,7 @@
 module PupilfirstXapi
   module Verbs
     COMPLETED             = Xapi.create_verb(id: 'http://adlnet.gov/expapi/verbs/completed', name: 'completed')
-    COMPLETED_ASSIGNMENT  = Xapi.create_verb(id: 'https://w3id.org/xapi/dod-isd/verbs/completed-assignment', name: 'completed assignment')
+    COMPLETED_ASSIGNMENT  = Xapi.create_verb(id: 'http://adlnet.gov/expapi/verbs/attempted', name: 'attempted')
     REGISTERED            = Xapi.create_verb(id: 'http://adlnet.gov/expapi/verbs/registered', name: 'registered')
     VIDEO_STARTED         = Xapi.create_verb(id: 'http://activitystrea.ms/schema/1.0/start', name: 'start')
     VIDEO_ENDED           = Xapi.create_verb(id: 'http://adlnet.gov/expapi/verbs/watched', name: 'watched')
