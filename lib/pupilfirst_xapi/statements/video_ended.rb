2e74603f1b19d5d5fe8429bedac05367386669a0
Mohamad Abras <mohamad.abras@gmail.com>
2022-01-20 16:28:16 +0200

    add context & result to video ended

--- lib/pupilfirst_xapi/statements/video_ended.rb
+++ lib/pupilfirst_xapi/statements/video_ended.rb
@@ -9,9 +9,35 @@ module PupilfirstXapi
       def call(actor_id:, resource_id:)
         actor = @repository.call(:user, actor_id)
         target = @repository.call(:target, resource_id)
+        instructor = target.course.course_authors.first.user
+        instructor_agent = Xapi::Agent.new(name: instructor.name, mbox: "mailto:#{instructor.email}")
+        context_activities = Xapi.create_context_activities(
+          parent: [Xapi::Activity.new.tap { |obj| obj.id = PupilfirstXapi.uri_for.call(target.course) }]
+        )
+        context = Xapi.create_context(
+          instructor: instructor_agent,
+          platform: "ZAMN",
+          language: "ar-SA",
+          extensions: {
+            "http://id.tincanapi.com/extension/attempt-id" => 1,
+            "http://id.tincanapi.com/extension/browser-info": {
+              code_name:"Unknow",
+              name: "Unknow",
+              version: "Unknow"
+            }
+          },
+          context_activities: context_activities
+        )
+
         Xapi.create_statement(
           actor: Actors.agent(actor),
           verb: Verbs::VIDEO_ENDED,
+          context: context,
+          result: Xapi::Result.new(
+            score: Xapi::Score.new(),
+            success: true,
+            completion: true
+          ),
           object: Objects.video_end(target, @uri_for)
         )
       end
